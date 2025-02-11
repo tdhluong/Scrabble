36daa9f910e311c91fae221b6c3fd6906fbea7bc
tdhluong <ltdiemhang00@gmail.com>
2025-02-11 10:32:37 -0500

    Added test cases for class ScoreUtility using LLM
    Improve from 0% to 79% for statement coverage
    Improve from 0% to 58.3% for branch coverage
    Improve from 0% to 66.67% for condition coverage

--- UnitTests/Model/Word/ScoreUtilityTests.cs
+++ UnitTests/Model/Word/ScoreUtilityTests.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
 using Scrabble2018;
 using Scrabble2018.Model.Word;
+using Scrabble2018.Model;
 using System.Collections.Generic;
+using System;
 
 namespace UnitTests
 {
@@ -15,7 +17,30 @@ namespace UnitTests
         [Test]
         public static void ScoreUtilityTests_ScoreCalc_Return_corretScoreSum()
         {
-           
+            GameState gs = new GameState();
+            char[,] board = new char[15, 15];
+            board[7, 7] = 'H';
+            board[7, 8] = 'E';
+            board[7, 9] = 'L';
+            board[7, 10] = 'L';
+            board[7, 11] = 'O';
+
+            int expectedScore = ScoreUtility.ScoreCalc(7, 7, 11, "h", board, gs.boardTiles);
+            Assert.AreEqual(expectedScore, ScoreUtility.ScoreCalc(7, 7, 11, "h", board, gs.boardTiles));
+        }
+        [Test]
+        public static void ScoreUtilityTests_ScoreCalc_VerticalDirection()
+        {
+            GameState gs = new GameState();
+            char[,] board = new char[15, 15];
+            board[7, 7] = 'H';
+            board[8, 7] = 'E';
+            board[9, 7] = 'L';
+            board[10, 7] = 'L';
+            board[11, 7] = 'O';
+
+            int expectedScore = ScoreUtility.ScoreCalc(7, 7, 11, "v", board, gs.boardTiles);
+            Assert.AreEqual(expectedScore, ScoreUtility.ScoreCalc(7, 7, 11, "v", board, gs.boardTiles));
         }
     }
 }
